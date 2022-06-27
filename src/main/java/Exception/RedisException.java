26672e3812afa6aa6bd2d77a7ee90779d8c3d919
ruitianzhong <zhongruitian2003@qq.com>
2022-06-27 09:48:10 +0800

    add RedisException

---
+++ src/main/java/Exception/RedisException.java
@@ -0,0 +1,22 @@
+package Exception;
+
+/**
+ * @author ruitianzhong
+ * @email zhongruitian2003@qq.com
+ */
+public class RedisException extends RuntimeException {
+    public static final long serialVersionUID = 1L;
+
+    public RedisException(String message) {
+        super(message);
+    }
+
+    public RedisException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public RedisException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
